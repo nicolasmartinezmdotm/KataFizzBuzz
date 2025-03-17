40328d08cfacd72a16ee3655c4701daf804537d4
Nico <n@n.n>
2025-03-17 17:20:37 +0100

    First part of the exercise

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(testNumber(i));
+        }
+    }
+
+    private static String testNumber(int n) {
+        if (n % 3 == 0) {
+            return "Fizz";
+        }
+
+        if (n % 5 == 0) {
+            return "Buzz";
+        }
+
+        return n + "";
     }
 }
